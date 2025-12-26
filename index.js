719d10107924d918ab73a69c75cbeaacee47ba73
roaayala <roaayala.ahm@gmail.com>
2025-12-26 22:05:36 +0800

    stuck

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
 
-// score
-let computerScore = 0;
-let humanScore = 0;
-
 function getComputerChoice() {
 	const randomChoice = Math.trunc(Math.random() * 3 + 1);
 	switch (randomChoice) {
@@ -25,28 +21,43 @@ function getHumanChoice() {
 	return randomChoice;
 }
 
-function playRound(computerChoice, humanChoice) {
-	console.log(computerChoice);
-	console.log(humanChoice);
-	if (computerChoice === 'rock' && humanChoice === 'rock') {
-		computerScore = computerScore;
-		humanScore = humanScore;
-		console.log(
-			`Draw, you both choose rock. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
-		);
-	} else if (computerChoice === 'paper' && humanChoice === 'paper') {
-		computerScore = computerScore;
-		humanScore = humanScore;
-		console.log(
-			`Draw, you both choose paper. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
-		);
-	} else if (computerChoice === 'scissors' && humanChoice === 'scissors') {
-		computerScore = computerScore;
-		humanScore = humanScore;
-		console.log(
-			`Draw, you both choose scissors. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
-		);
+function playGame() {
+	// score
+	let computerScore = 0;
+	let humanScore = 0;
+
+	function playRound(computerChoice, humanChoice) {
+		if (computerChoice === 'rock' && humanChoice === 'rock') {
+			computerScore = computerScore;
+			humanScore = humanScore;
+			console.log(
+				`Draw, you both choose rock. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
+			);
+		} else if (computerChoice === 'paper' && humanChoice === 'paper') {
+			computerScore = computerScore;
+			humanScore = humanScore;
+			console.log(
+				`Draw, you both choose paper. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
+			);
+		} else if (computerChoice === 'scissors' && humanChoice === 'scissors') {
+			computerScore = computerScore;
+			humanScore = humanScore;
+			console.log(
+				`Draw, you both choose scissors. Current score is ${computerScore} for Computer and ${humanScore} for Human.`
+			);
+		} else {
+			console.log(computerChoice);
+			console.log(humanChoice);
+		}
+	}
+
+	if (computerScore === 5) {
+		console.log('Computer Won');
+	} else if (humanScore === 5) {
+		console.log('Human Won');
+	} else {
+		playRound(computerChoice, humanChoice);
 	}
 }
 
-playRound(computerChoice, humanChoice);
+playGame();
