3f7f2b425864a3954ffeca849c189de0803f7dcc
roaayala <roaayala.ahm@gmail.com>
2025-12-26 21:27:06 +0800

    get computer and human choice: done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('rock paper scissors start');
+// choice
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
+
+// score
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+	const randomChoice = Math.trunc(Math.random() * 3 + 1);
+	switch (randomChoice) {
+		case 1:
+			return 'rock';
+		case 2:
+			return 'paper';
+		case 3:
+			return 'scissors';
+	}
+}
+
+function getHumanChoice() {
+	let randomChoice = prompt(
+		'Choose between Rock, Paper or Scissors'
+	).toLocaleLowerCase();
+
+	return randomChoice;
+}
+
+function playRound(computerChoice, humanChoice) {}
